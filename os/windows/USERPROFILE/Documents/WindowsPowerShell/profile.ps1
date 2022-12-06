eb140a3bdf292e5563cfd15b1954f8906e0b2d02
Vyacheslav Dmitriev <rtefforg@gmail.com>
2022-12-06 21:12:59 +0300

    Update: profile (PowerShell)

--- os/windows/USERPROFILE/Documents/WindowsPowerShell/profile.ps1
+++ os/windows/USERPROFILE/Documents/WindowsPowerShell/profile.ps1
@@ -1,10 +1,62 @@
-function Download-Audio ([string]$url, [string]$format="mp3") {
-    echo "Downloading audio ($format) from $url..."
-    youtube-dl --no-check-certificate --extract-audio --embed-thumbnail --audio-format $format $url ; echo "Done"
+#                       Functions
+# ---------------------------------------------------------
+
+function Get-Audio ([string]$url, [string]$format = "mp3") {
+    Write-Output "Downloading audio ($format) from $url..."
+    youtube-dl --no-check-certificate --extract-audio --audio-format $format $url ; Write-Output "Done"
+}
+
+function Get-Video ([string] $url) {
+    Write-Output "Downloading video from ${url}..."
+    youtube-dl --no-check-certificate -f bestvideo+bestaudio $url ; Write-Output "Done"
+}
+
+function New-File() {
+    foreach ($fileName in $args) {
+        if (-not(Test-Path $fileName)) {
+            New-Item -ItemType File -Name $fileName
+        }
+        else {
+            (Get-ChildItem $fileName).LastWriteTime = Get-Date
+        }
+    }
 }
 
 
-function Download-Video ([string] $url) {
-    echo "Downloading video from ${url}..."
-    youtube-dl --no-check-certificate -f bestvideo+bestaudio $url ; echo "Done"
+#                  Docker + Django func
+# ---------------------------------------------------------
+
+function New-DockerDjangoMigrations {
+    docker-compose run web python3 manage.py makemigrations
 }
+
+function New-DockerDjangoMigrate {
+    docker-compose run web python3 manage.py migrate
+}
+
+function New-DockerDjangoTest {
+    docker-compose run web pytest
+}
+
+function New-DockerDjangoShellPlus {
+    docker-compose run web python manage.py shell_plus
+}
+
+
+#                       Aliases
+# ---------------------------------------------------------
+Set-Alias -Name c -Value clear
+Set-Alias -Name Touch New-File
+
+Set-Alias -Name dj-migrations New-DockerDjangoMigrations
+Set-Alias -Name dj-migrate New-DockerDjangoMigrate
+Set-Alias -Name dj-test New-DockerDjangoTest
+Set-Alias -Name dj-shell New-DockerDjangoShellPlus
+
+
+#                       Keybindings
+# ---------------------------------------------------------
+Set-PSReadLineKeyHandler -Key "ctrl+a" -ScriptBlock { [Microsoft.PowerShell.PSConsoleReadLine]::BeginningOfLine() }
+Set-PSReadLineKeyHandler -Key "ctrl+e" -ScriptBlock { [Microsoft.PowerShell.PSConsoleReadLine]::EndOfLine() }
+Set-PSReadLineKeyHandler -Key "ctrl+y" -ScriptBlock { [Microsoft.PowerShell.PSConsoleReadLine]::Yank() }
+Set-PSReadLineKeyHandler -Key "ctrl+u" -ScriptBlock { [Microsoft.PowerShell.PSConsoleReadLine]::Undo() }
