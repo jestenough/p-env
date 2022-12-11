4b997206bb2da91287e91fcdfa685e39f1bb3632
Vyacheslav Dmitriev <rtefforg@gmail.com>
2022-12-11 19:15:05 +0300

    Update: python packages

--- os/windows/install/python_packages.ps1
+++ os/windows/install/python_packages.ps1
@@ -2,17 +2,15 @@ function Install-PythonPackages {
     python -m pip install --upgrade pip
 
     python -m pip install pipenv
-    python -m pip install requests
+    python -m pip install pylint
     python -m pip install pytest
-
+    python -m pip install requests
     python -m pip install notebook
 }
-
 
 function Main {
     $ErrorActionPreference = "Stop"
     Install-PythonPackages
 }
-
 
 Main
